b11aea29a17e65928786a23aab26ac6239b41d10
HMTD-Links <109021412+HMTD-Links@users.noreply.github.com>
2023-11-18 17:22:23 +0530

    Update modify.py

--- ChannelBot/modify.py
+++ ChannelBot/modify.py
@@ -3,12 +3,7 @@ from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup
 from pyrogram.errors.exceptions import FloodWait
 from ChannelBot.database.channel_sql import (
-    get_caption,
-    get_position,
-    get_buttons,
-    get_webpage_preview,
-    get_sticker,
-    get_edit_mode
+    get_buttons
 )
 from ChannelBot.string_to_buttons import string_to_buttons
 
@@ -23,44 +18,19 @@ from ChannelBot.string_to_buttons import string_to_buttons
 @Client.on_message(filters.channel & ~filters.forwarded)
 async def modify(_, msg: Message):
     channel_id = msg.chat.id
-    caption = await get_caption(channel_id)
-    sticker = await get_sticker(channel_id)
-    edit_mode = await get_edit_mode(channel_id)
-    if edit_mode == 'media' and not msg.media:
-        return
+    buttons = await get_buttons(channel_id)
     try:
-        if caption:
-            position = await get_position(channel_id)
-            buttons = await get_buttons(channel_id)
             if buttons:
                 buttons = await string_to_buttons(buttons)
-            webpage_preview = await get_webpage_preview(channel_id)
-            if position == 'above':
-                if msg.caption:
-                    caption += '\n\n' + msg.caption.markdown
-                elif msg.text:
-                    caption += '\n\n' + msg.text.markdown
-            elif position == 'below':
-                if msg.caption:
-                    caption = msg.caption.markdown + '\n\n' + caption
-                elif msg.text:
-                    caption = msg.text.markdown + '\n\n' + caption
-            if webpage_preview:
-                disable_webpage_preview = False
-            else:
-                disable_webpage_preview = True
             if buttons:
                 await msg.edit_text(
                     caption,
-                    reply_markup=InlineKeyboardMarkup(buttons),
-                    disable_web_page_preview=disable_webpage_preview
+                    reply_markup=InlineKeyboardMarkup(buttons)
                 )
             else:
                 await msg.edit_text(
                     caption,
                     disable_web_page_preview=disable_webpage_preview
                 )
-        if sticker:
-            await msg.reply_sticker(sticker, quote=False)
     except FloodWait as e:
         await asyncio.sleep(e.x)
